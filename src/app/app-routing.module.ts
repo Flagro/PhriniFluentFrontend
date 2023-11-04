b51b45003b4d4c2a41dcbd8a22b294b1739b4375
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-04 20:30:38 +0100

    implemented testing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
+import { MainComponent } from './pages/main/main.component';
+import { WordGroupsComponent } from './pages/word-groups/word-groups.component';
+import { QuizComponent } from './pages/quiz/quiz.component';
+import { AboutComponent } from './pages/about/about.component';
+import { UserSettingsComponent } from './pages/user-settings/user-settings.component';
 
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'word-groups', component: WordGroupsComponent },
+  { path: 'quiz', component: QuizComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'user-settings', component: UserSettingsComponent },
+  // ... other routes
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }

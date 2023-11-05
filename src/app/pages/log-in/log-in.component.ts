1c3f6df22a94c5b2d11b75d38fbbe9fce831b92e
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-05 12:52:25 +0100

    log in sign up logic template

--- src/app/pages/log-in/log-in.component.ts
+++ src/app/pages/log-in/log-in.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
-  selector: 'app-log-in',
-  templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.scss']
+    selector: 'app-log-in',
+    templateUrl: './log-in.component.html',
+    styleUrls: ['./log-in.component.scss']
 })
 export class LogInComponent {
+    constructor(private authService: AuthService) {}
 
+    onLogin() {
+        // Implement log-in logic
+    }
 }
--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
-  selector: 'app-sign-up',
-  templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+    selector: 'app-sign-up',
+    templateUrl: './sign-up.component.html',
+    styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+    constructor(private authService: AuthService) {}
 
+    onSignUp() {
+        // Implement sign-up logic
+    }
 }

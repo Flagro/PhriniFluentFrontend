ba0f876f94f6ca08d4d7f464047a7d73784cf477
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-05 12:49:56 +0100

    modified auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+    private isLoggedIn = false; // This should eventually be linked to your OAuth2 backend
 
-  constructor() { }
+    // Simulate a login - this will be replaced with real authentication logic
+    login(username: string, password: string): Observable<boolean> {
+        // Placeholder for actual authentication
+        this.isLoggedIn = true; // Assume login is successful
+        return of(this.isLoggedIn);
+    }
+
+    // Simulate a logout
+    logout(): void {
+        this.isLoggedIn = false;
+    }
+
+    // Check the login status
+    checkLoggedIn(): Observable<boolean> {
+        return of(this.isLoggedIn);
+    }
 }

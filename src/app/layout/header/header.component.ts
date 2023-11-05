d32206066dd3b0863356c65ec9c6323fe957ac03
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-05 18:39:02 +0100

    auth logic in header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service'; // Update the path as per your structure
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isLoggedIn = false;
 
+  constructor(private authService: AuthService, private router: Router) {
+    // Subscribe to authentication state changes
+    this.authService.authState.subscribe((state) => {
+      this.isLoggedIn = state;
+    });
+  }
+
+  // Optional: implement logout functionality
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }

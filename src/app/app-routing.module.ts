890d102e0fcc90be4187fa2d141335fc61790fe4
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-07 00:18:13 +0100

    styled sign up page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { QuizComponent } from './pages/quiz/quiz.component';
 import { AboutComponent } from './pages/about/about.component';
 import { UserSettingsComponent } from './pages/user-settings/user-settings.component';
 import { LogInComponent } from './pages/log-in/log-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'user-settings', component: UserSettingsComponent },
   { path: 'login', component: LogInComponent },
+  { path: 'signup', component: SignUpComponent },
 ];
 
 @NgModule({

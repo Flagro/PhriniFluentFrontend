f4fb1cb25aee0d553f19d93718de6aeb8cbdbb98
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-07 14:01:10 +0100

    implemented group_id routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './pages/main/main.component';
 import { WordGroupsComponent } from './pages/word-groups/word-groups.component';
+import { PublicGroupComponent } from './pages/public-group/public-group.component';
+import { PrivateGroupComponent } from './pages/private-group/private-group.component';
 import { AboutComponent } from './pages/about/about.component';
 import { UserSettingsComponent } from './pages/user-settings/user-settings.component';
 import { LogInComponent } from './pages/log-in/log-in.component';
@@ -10,7 +12,10 @@ import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
-  { path: 'groups', component: WordGroupsComponent },
+  { path: 'groups', component: WordGroupsComponent, children: [
+    { path: 'public/:groupId', component: PublicGroupComponent },
+    { path: 'private/:groupId', component: PrivateGroupComponent }
+  ]},
   { path: 'about', component: AboutComponent },
   { path: 'settings', component: UserSettingsComponent },
   { path: 'login', component: LogInComponent },
--- src/app/pages/public-group/public-group.component.ts
+++ src/app/pages/public-group/public-group.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-public-group',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./public-group.component.scss']
 })
 export class PublicGroupComponent {
+  groupId: string;
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.groupId = params.get('groupId');
+      // Now you can use this.groupId to load the specific group data
+    });
+  }
 }

604d88a4fa7689dcab4a1864c44292541d2034f7
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-09 19:07:43 +0100

    updated urls

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
@@ -12,9 +12,12 @@ export interface AuthResponse {
   providedIn: 'root'
 })
 export class AuthService {
+  private loginUrl = `${environment.apiEndpoint}/phrini_fluent_users/login/`; // Use environment variable
+  private signupUrl = `${environment.apiEndpoint}/phrini_fluent_users/signup/`; // Use environment variable
 
-  private loginUrl = `${environment.apiEndpoint}/login`; // Use environment variable
-  private signupUrl = `${environment.apiEndpoint}/signup`; // Use environment variable
+  // BehaviorSubject to hold the current auth state
+  private authStateSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(this.hasToken());
+  authState: Observable<boolean> = this.authStateSubject.asObservable();
 
   constructor(private http: HttpClient) {}
 
@@ -28,6 +31,22 @@ export class AuthService {
     return this.http.post<AuthResponse>(this.signupUrl, { name, surname, email, username, password }).pipe(
       catchError(this.handleError)
     );
+  }
+
+  // Method to update auth state
+  setAuthState(value: boolean) {
+    this.authStateSubject.next(value);
+  }
+
+  // Method to check if token exists
+  hasToken(): boolean {
+    return !!localStorage.getItem('auth_token');
+  }
+
+  // Method to log out
+  logout() {
+    localStorage.removeItem('auth_token');
+    this.setAuthState(false); // Update authState to false when logged out
   }
 
   private handleError(error: any) {

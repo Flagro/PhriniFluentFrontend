6e949d881457780c47cfd7c7b3ef27ace8c508bf
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-09 19:07:58 +0100

    updated log in logic

--- src/app/pages/log-in/log-in.component.ts
+++ src/app/pages/log-in/log-in.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-log-in',
-    templateUrl: './log-in.component.html',
-    styleUrls: ['./log-in.component.scss']
+  selector: 'app-login',
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.scss']
 })
 export class LogInComponent {
-    constructor(private authService: AuthService) {}
+  username: string = '';
+  password: string = '';
 
-    onLogin() {
-        // Implement log-in logic
-    }
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login(this.username, this.password).subscribe(
+      data => {
+        localStorage.setItem('auth_token', data.token);
+        this.router.navigate(['/']); // replace '/dashboard' with the route you want to navigate to after login
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
+  onSubmit() {
+    // Call the login method when the form is submitted
+    this.login();
+  }
 }

c2dfa5662b4a364b3cdd12c6977772b5082ca7b2
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-09 17:51:17 +0100

    implemented auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,46 +1,23 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private isAuthenticatedSubject: BehaviorSubject<boolean>;
 
-  constructor() {
-    // Initialize with `false` indicating the user is not logged in.
-    // In a real application, you might want to check for a token in localStorage here.
-    this.isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
+  private loginUrl = `${environment.apiEndpoint}/api/login`; // Use environment variable
+  private signupUrl = `${environment.apiEndpoint}/api/signup`; // Use environment variable
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(this.loginUrl, { username, password });
   }
 
-  // Observable to be used by components to reactively respond to authentication changes
-  get authState(): Observable<boolean> {
-    return this.isAuthenticatedSubject.asObservable();
-  }
-
-  // Call this method when the user attempts to log in
-  login(username: string, password: string): Observable<boolean> {
-    // Placeholder for actual login logic
-    // In a real application, you would make an HTTP request here
-    // For this example, let's assume the user is authenticated if the username and password are 'admin'
-    const isAuthenticated = username === 'admin' && password === 'admin';
-    
-    // Update the auth state
-    this.isAuthenticatedSubject.next(isAuthenticated);
-
-    return of(isAuthenticated);
-  }
-
-  // Call this method when the user logs out
-  logout(): void {
-    // Clear user authentication (e.g., remove the token from localStorage)
-    // Update the auth state
-    this.isAuthenticatedSubject.next(false);
-  }
-
-  // Helper method to check if the user is currently authenticated
-  checkLoggedIn(): boolean {
-    // In a real application, you might check for the presence of a token here
-    return this.isAuthenticatedSubject.value;
+  signup(name: string, surname: string, email: string, username: string, password: string): Observable<any> {
+    return this.http.post(this.signupUrl, { name, surname, email, username, password });
   }
 }

3517175dee8024e9943771fdbc16d2d399066c84
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-17 14:00:32 +0100

    updated ts for user settings

--- src/app/pages/user-settings/user-settings.component.ts
+++ src/app/pages/user-settings/user-settings.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-settings',
@@ -11,20 +13,43 @@ export class UserSettingsComponent {
   email = '';
   username = '';
 
-  constructor() {
-    // Initialize your variables here
-    // For example, you might want to load the current user's settings
+  constructor(private authService: AuthService, private router: Router) {
+    // Load current user's settings here
+    // This could involve a call to the authService or another service
+    // For example:
+    // this.loadUserSettings();
+  }
+
+  loadUserSettings() {
+    // Implement logic to load the user's current settings
+    // For example, use authService to fetch the details
+  }
+
+  saveSettings() {
+    // Implement logic to save the user settings
+    // This could involve sending the updated details to your backend via authService
+    console.log('Saving user settings', { name: this.name, surname: this.surname, email: this.email, username: this.username });
+    // Example:
+    // this.authService.updateUserSettings({name: this.name, ...}).subscribe(
+    //   response => {
+    //     // Handle successful update
+    //   },
+    //   error => {
+    //     console.error(error);
+    //   }
+    // );
   }
 
   onSubmit() {
-    // Implement your logic to save the settings
-    // For example, send a request to your backend service
-    console.log('Saving user settings', { name: this.name, surname: this.surname, email: this.email, username: this.username });
+    // Call saveSettings method when the form is submitted
+    this.saveSettings();
   }
 
   signOut() {
-    // Implement your logic for signing out
+    // Implement the sign-out logic
+    // Typically, this would clear any authentication tokens and redirect the user
     console.log('Signing out user');
-    // For example, clear the authentication token and redirect to login page
+    localStorage.removeItem('auth_token');
+    this.router.navigate(['/login']); // Replace with your login route
   }
 }

999d9fa0fa7ad3d834c13f2ed94d3b45baee3eac
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-17 13:59:18 +0100

    implemented base for user settings component

--- src/app/pages/user-settings/user-settings.component.ts
+++ src/app/pages/user-settings/user-settings.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-settings.component.scss']
 })
 export class UserSettingsComponent {
+  name = '';
+  surname = '';
+  email = '';
+  username = '';
 
+  constructor() {
+    // Initialize your variables here
+    // For example, you might want to load the current user's settings
+  }
+
+  onSubmit() {
+    // Implement your logic to save the settings
+    // For example, send a request to your backend service
+    console.log('Saving user settings', { name: this.name, surname: this.surname, email: this.email, username: this.username });
+  }
+
+  signOut() {
+    // Implement your logic for signing out
+    console.log('Signing out user');
+    // For example, clear the authentication token and redirect to login page
+  }
 }

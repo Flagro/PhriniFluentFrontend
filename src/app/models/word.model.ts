56b6f889ed273626352a13ecf0725abe52f9105e
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-24 18:07:18 +0100

    added support for multiple word descriptions

--- src/app/models/word.model.ts
+++ src/app/models/word.model.ts
@@ -1,4 +1,12 @@
 export interface Word {
     id: number;
     text: string;
+    descriptions: Description[];
+}
+
+interface Description {
+    description_text: string;
+    language: {
+      language_name: string;
+    };
 }

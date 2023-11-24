c2e7340f6cf2c8991e0501e50c1879e9a8a17aa4
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-24 18:17:58 +0100

    implemented quiz component

--- src/app/pages/quiz/quiz.component.ts
+++ src/app/pages/quiz/quiz.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { WordService } from '../../services/words.service';
+import { Word } from '../../models/word.model';
+import { WordGroup } from '../../models/word-group.model';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.scss']
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit {
+  word: Word | null = null;
+  wordGroup: WordGroup | null = null;
+  userInput: string = '';
 
+  constructor(
+    private route: ActivatedRoute,
+    private wordService: WordService
+  ) { }
+
+  ngOnInit(): void {
+    const groupIdParam = this.route.snapshot.paramMap.get('group_id');
+    if (groupIdParam) {
+      const groupId = +groupIdParam;
+      this.fetchRandomWord(groupId);
+      // Fetch word group details as needed
+    } else {
+      // Handle the case where groupId is null
+      // For example, redirecting to a 'not found' page or showing an error message
+      console.error('Group ID is missing');
+      // Redirect to home or another appropriate page
+      window.location.href = '/';
+    }
+  }
+
+  fetchRandomWord(groupId: number): void {
+    this.wordService.getRandomWord(groupId).subscribe(data => {
+      this.word = data;
+    });
+  }
+
+  checkAnswer(): void {
+    // Implement your logic to check the answer
+    console.log(this.userInput); // For now, just log the input
+  }
 }

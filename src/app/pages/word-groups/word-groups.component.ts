91331c17122e3b696a3abb61ec171bc38df029b3
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-24 13:33:58 +0100

    implemented word groups component ts

--- src/app/pages/word-groups/word-groups.component.ts
+++ src/app/pages/word-groups/word-groups.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { WordGroupService } from '../../services/word-groups.service';
+import { WordGroup } from '../../models/word-group.model';
 
 @Component({
   selector: 'app-word-groups',
   templateUrl: './word-groups.component.html',
   styleUrls: ['./word-groups.component.scss']
 })
-export class WordGroupsComponent {
+export class WordGroupsComponent implements OnInit {
+  publicWordGroups: WordGroup[] = [];
+  privateWordGroups: WordGroup[] = [];
+  isLoggedIn = false;
 
+  constructor(private authService: AuthService, private wordGroupService: WordGroupService) {}
+
+  ngOnInit() {
+    this.isLoggedIn = this.authService.isLoggedIn();
+    this.loadWordGroups();
+  }
+
+  loadWordGroups() {
+    this.wordGroupService.getPublicWordGroups('en').subscribe(groups => {
+      this.publicWordGroups = groups;
+    });
+
+    if (this.isLoggedIn) {
+      this.wordGroupService.getPrivateWordGroups('en').subscribe(groups => {
+        this.privateWordGroups = groups;
+      });
+    }
+  }
 }

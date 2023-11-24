bbd4fbac88d42122370e179bafa0df99a14b6941
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-24 16:33:43 +0100

    word group model update

--- src/app/models/word-group.model.ts
+++ src/app/models/word-group.model.ts
@@ -3,5 +3,13 @@ import { Word } from './word.model';
 export interface WordGroup {
   id: number;
   name: string;
+  descriptions: Description[];
   words: Word[];
+}
+
+interface Description {
+  description_text: string;
+  language: {
+    language_name: string;
+  };
 }

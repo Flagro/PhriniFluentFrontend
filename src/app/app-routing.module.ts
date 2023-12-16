9c53628b82a0e55fd5e0c63758af7bef82790b5d
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-12-16 13:28:53 +0100

    removed public and private group pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,6 @@ import { AuthGuard } from './guards/auth.guard';
 
 import { MainComponent } from './pages/main/main.component';
 import { WordGroupsComponent } from './pages/word-groups/word-groups.component';
-import { PublicGroupComponent } from './pages/public-group/public-group.component';
-import { PrivateGroupComponent } from './pages/private-group/private-group.component';
 import { QuizComponent } from './pages/quiz/quiz.component';
 import { AboutComponent } from './pages/about/about.component';
 import { UserSettingsComponent } from './pages/user-settings/user-settings.component';
@@ -16,10 +14,7 @@ import { SignupSuccessComponent } from './pages/signup-success/signup-success.co
 
 const routes: Routes = [
   { path: '', component: MainComponent },
-  { path: 'groups', component: WordGroupsComponent, children: [
-    { path: 'public/:groupId', component: PublicGroupComponent },
-    { path: 'private/:groupId', component: PrivateGroupComponent }
-  ]},
+  { path: 'groups', component: WordGroupsComponent},
   { path: 'quiz/:group_id', component: QuizComponent },
   { path: 'about', component: AboutComponent },
   { path: 'settings', component: UserSettingsComponent, canActivate: [AuthGuard] },
